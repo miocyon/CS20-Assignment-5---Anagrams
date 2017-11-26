99588cd6189ad04eae86c293d2ef156b6b2fd7e8
miocyon <miocyon@gmail.com>
2017-11-25 22:23:04 -0800

    Add fxn descriptions
    Added fxn descriptions from Assignment

--- AFriscia-CS20-4118-A5/anagramfxns.cpp
+++ AFriscia-CS20-4118-A5/anagramfxns.cpp
@@ -12,15 +12,26 @@ using namespace std;
 
 int readDictionary(istream &dictfile, string dict[])
 {
+//  Places each string in dictfile into the array dict. Returns the number of words read into dict.
+//  This number should not be larger than MAXDICTWORDS since that is the size of the array.
+    
     
 }
 
 int recursivePermute(string word, const string dict[], int size, string results[]);
 {
+//  Places all the permutations of word, which are found in dict into results.
+//  Returns the number of matched words found.
+//  This number should not be larger than MAXRESULTS since that is the size of the array.
+//  The size is the number of words inside the dict array.
+    
     
 }
 
 void recurPrint(const string results[], int size);
 {
+//  Prints size number of strings from results.
+//  The results can be printed in any order.
+
     
 }
