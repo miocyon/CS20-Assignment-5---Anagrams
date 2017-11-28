fc44750a46001bb17a69dc1ceb771159b028b124
miocyon <miocyon@gmail.com>
2017-11-27 21:02:45 -0800

    made readDictionary recursive
    It's what I was supposed to do...

--- AFriscia-CS20-4118-A5/anagramfxns.cpp
+++ AFriscia-CS20-4118-A5/anagramfxns.cpp
@@ -16,30 +16,47 @@ int readDictionary(istream &dictfile, string dict[])
 //  Places each string in dictfile into the array dict. Returns the number of words read into dict.
 //  This number should not be larger than MAXDICTWORDS since that is the size of the array.
     string linedump;
-    int dictcount = 0;
     
-    while(getline(dictfile, linedump))
+    if(!getline(dictfile, linedump))
     {
-        dict[dictcount] = linedump;
-        dictcount++;
+        return 0;
     }
-    return dictcount;
+    *dict = linedump;
+    cout << linedump << endl;
+    return 1 + readDictionary(dictfile, dict+1);
 }
 
-int recursivePermute(string word, const string dict[], int size, string results[]);
-{
-//  Places all the permutations of word, which are found in dict into results.
-//  Returns the number of matched words found.
-//  This number should not be larger than MAXRESULTS since that is the size of the array.
-//  The size is the number of words inside the dict array.
-    
-    
-}
+//int recursivePermute(string word, const string dict[], int size, string results[]);
+//{
+////  Places all the permutations of word, which are found in dict into results.
+////  Returns the number of matched words found.
+////  This number should not be larger than MAXRESULTS since that is the size of the array.
+////  The size is the number of words inside the dict array.
+//    if (word.size() == 0)
+//    {
+//        cout << out << endl;
+//        return;
+//        }
+//        
+//        // One be one move all characters at
+//        // the beginning of out (or result)
+//        for (int i = 0; i < word.size(); i++)
+//        {
+//            // Remove first character from str and
+//            // add it to out
+//            recursivePermute(word.substr(1), out + str[0]);
+//            
+//            // Rotate string in a way second character
+//            // moves to the beginning.
+//            rotate(word.begin(), word.begin() + 1, word.end());
+//        }
+//    
+//}
 
-void recurPrint(const string results[], int size);
-{
-//  Prints size number of strings from results.
-//  The results can be printed in any order.
-
-    
-}
+//void recurPrint(const string results[], int size);
+//{
+////  Prints size number of strings from results.
+////  The results can be printed in any order.
+//
+//    
+//}
